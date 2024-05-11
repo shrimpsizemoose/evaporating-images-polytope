0512cae4bbceb88a917b8bf2a1ab7dbe414a5c2f
Igor Mosyagin <408698+cra@users.noreply.github.com>
2024-05-11 15:55:46 +0200

    Change pixel filling logic so it feels more 'fair'

--- code/redis-updater/api/update_redis.py
+++ code/redis-updater/api/update_redis.py
@@ -27,18 +27,25 @@ def build_coords_data(coords, shiftX=0, shiftY=0, evaporate=False):
 
 def update_coords_in_redis(url, evaporate=False):
     r = redis.from_url(url)
-    r.flushdb()
 
     glider = draw_glider()
     moose = draw_moose()
 
     # moose can go
     pixels = build_coords_data(moose, shiftX=7, shiftY=5)
-    for (y, x), v in pixels.items():
+    if evaporate:
+        pixels = list(pixels.items())
+        random.shuffle(pixels)
+    limit = 70
+    for i, ((y, x), v) in enumerate(pixels):
         key = f'coords:{y}:{x}'
+        if r.exists(key):
+            continue
         r.hset(key, mapping=v)
         if evaporate:
-            r.expire(key, int(10 * random.random()))
+            r.expire(key, int(20 * random.random()))
+        if i == limit:
+            break
 
     # the glider stays
     pixels = build_coords_data(glider, shiftY=1)
