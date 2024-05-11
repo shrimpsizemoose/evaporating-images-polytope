ea84be666f9cca78a306a274f034e4de8e8f95f4
Igor Mosyagin <408698+cra@users.noreply.github.com>
2024-05-11 13:17:57 +0200

    Add frontend code

---
+++ code/frontend/grid.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    const p = 10;
+    const cw = 20;
+    const bw = 500;
+    const bh = 500;
+
+    const cols = bw / cw;
+    const rows = bh / cw;
+
+    canvas.width = bw + 2*p;
+    canvas.height = bh + 2*p;
+    // drawGrid(ctx);
+
+    function drawGrid(ctx) {
+        ctx.strokeStyle = '#ccc';
+        for (let x = 0; x <= bw; x+= cw) {
+            ctx.beginPath();
+            ctx.moveTo(p + x, p);
+            ctx.lineTo(p + x, p + bh);
+            ctx.stroke();
+        }
+        for (let y = 0; y <= bh; y+= cw) {
+            ctx.beginPath();
+            ctx.moveTo(p, y + p);
+            ctx.lineTo(p + bw, y + p);
+            ctx.stroke();
+        }
+    }
+
+    function drawMario() {
+        fetch("http://localhost:9099/coords/")
+        .then(response => response.json())
+        .then(data => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            data.coords.forEach(item => {
+                ctx.fillStyle = item.color;
+                // console.table(item);
+
+                const x = item.x * cw + p;
+                const y = item.y * cw + p;
+
+                if (item.draw) {
+                    ctx.fillRect(x, y, cw, cw);
+                }
+            });
+        })
+        .catch(err => console.error('Error fetching Mario data:', err));
+    }
+
+    setInterval(drawMario, 1000);
+});
+
+
+
